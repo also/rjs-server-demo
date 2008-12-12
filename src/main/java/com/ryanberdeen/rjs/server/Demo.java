0945c9bed7a69a570972a86281e7758d7a974686
Ryan Berdeen <ryan@ryanberdeen.com>
2008-12-12 00:08:46 -0800

    added simple demo handler

---
+++ src/main/java/com/ryanberdeen/rjs/server/Demo.java
@@ -0,0 +1,36 @@
+package com.ryanberdeen.rjs.server;
+
+import java.io.IOException;
+import java.util.HashSet;
+
+/**
+ * Demo {@link RjsHandler} that broadcasts all messages to all connected
+ * clients.
+ */
+public class Demo extends RjsHandlerAdapter  {
+	private HashSet<RjsClient> clients = new HashSet<RjsClient>();
+
+	@Override
+	public void clientConnected(RjsClient client) throws Exception {
+		clients.add(client);
+	}
+
+	@Override
+	public void clientDisconnected(RjsClient client) throws Exception {
+		clients.remove(client);
+	}
+
+	@Override
+	public void messageReceived(RjsClient client, String message) throws Exception {
+		for (RjsClient c : clients) {
+			c.send(message);
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		RjsServer server = new RjsServer();
+		server.setPort(1843);
+		server.setIoHandler(new Demo());
+		server.start();
+	}
+}
